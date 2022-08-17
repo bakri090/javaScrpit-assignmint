1752270c8dfd40e8a731414787ee962c0ae14867
bakri <bakri090@gmail.com>
2022-08-17 17:07:40 +0200

    a

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-var num1 = "10";
-var num2 = "20";
-console.log("1020");
-console.log(typeof "1020");
-console.log(`${1020}`);
-console.log(typeof `${1020}`);
-console.log(num1 +
-    "\n" + num2);
-console.log(`${num1}
-${num2}`);
+console.log("`I'm" + "In" + "\n" +
+    "\\\\" + "\n"
+    + "love" + "\\\\" + '\"""' + "\'''" + "\n"
+    + "\++ " + "with" + " ++" + "\n"
+    + '\\"""\\"""' + "\n" +
+    '""JavaScript""``');
